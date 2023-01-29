fecabbbfda7460f2392cf38760e9d01693e072d1
Filipe Bacof <filipebacof@gmail.com>
2023-01-29 11:55:14 -0300

    email funcionando + font monserrat + fixes

---
+++ script.js
@@ -0,0 +1,8 @@
+document.getElementById("contact-email").addEventListener("submit", function () {
+    var nome = this.querySelector("input[name=nome]"),
+        nome = nome.value;
+    var email = this.querySelector("input[name=email]"),
+        email = email.value;
+    var texto = "Olá destinatário, \nMeu nome é " + nome + " e meu email é " + email;
+    this.querySelector("input[name=Body]").setAttribute("value", texto);
+});
