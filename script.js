cbc6204e8f88205d979c9770510d4e7fe6167d75
Filipe Bacof01~ <filipebacof@gmail.com>
2023-01-30 19:26:48 -0300

    updated scroll

--- script.js
+++ script.js
@@ -6,3 +6,14 @@ document.getElementById("contact-email").addEventListener("submit", function () 
     var texto = "Olá destinatário, \nMeu nome é " + nome + " e meu email é " + email;
     this.querySelector("input[name=Body]").setAttribute("value", texto);
 });
+
+const contato = document.getElementById("contact-us");
+const positionOfElement = contato.getBoundingClientRect();
+
+function scrollContact() {
+    window.scrollTo({
+        top: positionOfElement.bottom,
+        left: 0,
+        behavior: "smooth",
+    })
+}
