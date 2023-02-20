dc5bf5f0f1031a875c5da3ac996c5314e8711612
Filipe Bacof <filipebacof@gmail.com>
2023-02-20 18:03:58 -0300

    svgs flags + language

--- script.js
+++ script.js
@@ -6,3 +6,6 @@ document.getElementById("contact-email").addEventListener("submit", function () 
     var texto = "Olá destinatário, \nMeu nome é " + nome + " e meu email é " + email;
     this.querySelector("input[name=Body]").setAttribute("value", texto);
 });
+
+const language = window.navigator.userLanguage;
+console.log(language);
