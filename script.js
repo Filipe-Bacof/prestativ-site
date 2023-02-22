dc221f0687fb89b585db4d9c17ac905bb0fdceeb
Filipe Bacof01~ <filipebacof@gmail.com>
2023-02-22 20:28:05 -0300

    updated PHP for send email

--- script.js
+++ script.js
@@ -17,3 +17,38 @@ function scrollContact() {
         behavior: "smooth",
     });
 }
+
+$(document).ready(function() {
+    $('#contact-email').submit(function(e) {
+        e.preventDefault(); // Previne o comportamento padrão do formulário (redirecionamento)
+
+        // Captura os dados do formulário
+        var nome = $('input[name="name"]').val();
+        var email = $('input[name="email"]').val();
+        var mensagem = $('input[name="message"]').val();
+
+        // Cria um objeto com os dados do formulário
+        var formData = {
+            'name': nome,
+            'email': email,
+            'message': mensagem
+        };
+
+        // Envia a requisição AJAX
+        $.ajax({
+            type: 'POST',
+            url: 'enviar.php',
+            data: formData,
+            dataType: 'json',
+            encode: true
+        })
+        .done(function(data) {
+            console.log(data);
+            alert('Mensagem enviada com sucesso!');
+        })
+        .fail(function(data) {
+            console.log(data);
+            alert('Ocorreu um erro ao enviar a mensagem.');
+        });
+    });
+});
